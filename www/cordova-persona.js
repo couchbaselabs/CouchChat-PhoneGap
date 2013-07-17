7a209c0536e98ba6131c186ce4781add48d38b4e
Chris Anderson <jchris@couchbase.com>
2013-07-16 20:01:31 -0700

    remove assets for plugin

--- www/cordova-persona.js
+++
@@ -1,8 +0,0 @@
-window.presentPersonaDialog = function(origin, callback) {
-  // use node.js style error reporting (first argument)
-  cordova.exec(function(assertion){
-    callback(false, assertion);
-  }, function(err) {
-   callback(err);
- }, "CBCordovaPersona", "presentPersonaDialog", [origin]);
-};
